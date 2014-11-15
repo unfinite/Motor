32364c3c3eb9c016fb1bc68b5ab1179b6c3af23e
Sergio Diaz <Alphapixels@users.noreply.github.com>
2014-11-15 09:07:53 -0400

    Bugfixes and improvements

--- motor.c
+++ motor.c
@@ -2,66 +2,13 @@
 /*
 *
 * Motor Wrappers
-* functions: move_preset, move
+* functions: move
 *
 */
 
-
-void move_preset(int preset)
-{
- 
-        switch(preset)
-        {
- 
-        case 1:
- 
-                // move right
-                motor[leftMotor]=100;
-                motor[rightMotor]=-100;
- 
-                break;
- 
-        case 2:
- 
-                // move left
-                motor[leftMotor]=-100;
-                motor[rightMotor]=100;
- 
-                break;
- 
-        case 3:
- 
-                // full power forwards
-                motor[leftMotor]=100;
-                motor[rightMotor]=100;
- 
-                break;
- 
-        case 4:
- 
-                // full power backwards
-                motor[leftMotor]=-100;
-                motor[rightMotor]=-100;
- 
-                break;
- 
-        case 5:
- 
-                // full stop
-                motor[leftMotor]= 0;
-                motor[rightMotor]= 0;
- 
-                break;
- 
-        }
- 
- 
-}
- 
-
 void move(int left, int right) {
 
-        motor[leftMotor]=left;
-        motor[rightMotor]=right;
+        motor(leftMotor)=left;
+        motor(rightMotor)=right;
 
 }
